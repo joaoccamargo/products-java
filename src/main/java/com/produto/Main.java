453069bae54c4dffbaf1b7c6e693f9d32029ec87
João Camargo <jclaudio00_12@hotmail.com>
2022-11-29 20:19:12 -0300

    encapsulamento
    private

--- src/main/java/com/produto/Main.java
+++ src/main/java/com/produto/Main.java
@@ -11,29 +11,30 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         Product product = new Product();
-        System.out.println("Enter product data:");
-        System.out.println("Name:");
-        product.name = sc.nextLine();
-        System.out.println("Price:");
-        product.price = sc.nextDouble();
-        System.out.println("Quantity in stock:");
-        product.quantity = sc.nextInt();
+
+        System.out.println("Enter product data: ");
+
+        System.out.print("Name: ");
+        product.setName(sc.nextLine());
+
+        System.out.print("Price: ");
+        product.setPrice(sc.nextDouble());
+
+        System.out.print("Quantity in stock: ");
+        product.setQuantity(sc.nextInt());
 
         System.out.println();
         System.out.println("Product data: " + product);
-
-        System.out.println("Enter the number of products to be added in stock: ");
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
         int quantity = sc.nextInt();
         product.addProducts(quantity);
-
         System.out.println();
         System.out.println("Updated data: " + product);
-
         System.out.println();
-        System.out.println("Enter the number of products to be removed from stock: ");
+        System.out.print("Enter the number of products to be removed from stock: ");
         quantity = sc.nextInt();
         product.removeProducts(quantity);
-
         System.out.println();
         System.out.println("Updated data: " + product);
 
--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -1,10 +1,33 @@
 package entities;
 
 public class Product {
+    private String name;
+    private double price;
+    private int quantity;
 
-    public String name;
-    public double price;
-    public int quantity;
+    public Product(){
+
+    }
+
+    public Product(String name, double price, int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
 
     public double totalValueInStock(){
         return price * quantity;
